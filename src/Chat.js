032172231c4dbd4c93dd5b87fb6486c43d37af64
Andrew Burkholder <hi@andrew.vip>
2018-07-03 15:49:30 -0400

    Add a (hard-coded) message when submitted

--- src/Chat.js
+++ src/Chat.js
@@ -2,13 +2,57 @@ import React, { Component } from 'react'
 
 import ChatHeader from './ChatHeader'
 import MessageList from './MessageList'
+import MessageForm from './MessageForm'
 
 class Chat extends Component {
-  render() {
+    constructor () {
+        super()
+
+        this.state = {
+            messages: [
+                {id: 1,
+                user: {
+                    uid: 'kdjkjfkdjf',
+                    displayName: "Andrew",
+                    email: 'hi@andrew.vip'
+                },
+                body: 'Chatting up a storm, yo!'
+                },
+        
+                {id: 2,
+                user: {
+                    uid: 'kdj23jkdjf',
+                    displayName: "Davey",
+                    email: 'davey@fretless.com'
+                },
+                body: 'Too much chatting.'
+                }
+            ]
+        }
+    }
+
+    addMessage = () => {
+        const messages = [...this.state.messages]
+
+        messages.push({
+            id: Date.now(),
+            user: {
+                uid: 'kdj090jkdjf',
+                displayName: "Stefany",
+                email: 'stephanie@singing.org'
+            },
+            body: 'I do not think we\'re chatting too much!',
+        })
+
+        this.setState({ messages: messages })
+    }
+
+    render() {
     return (
       <div className="Chat">
         <ChatHeader />
-        <MessageList />
+        <MessageList messages={this.state.messages}/>
+        <MessageForm addMessage={this.addMessage}/>
       </div>
     )
   }
--- src/MessageList.js
+++ src/MessageList.js
@@ -2,30 +2,10 @@ import React from 'react'
 
 import Message from './Message'
 
-const MessageList = () => {
-    const messages = [
-        {id: 1,
-        user: {
-            uid: 'kdjkjfkdjf',
-            displayName: "Andrew",
-            email: 'hi@andrew.vip'
-        },
-        body: 'Chatting up a storm, yo!'
-        },
-
-        {id: 2,
-        user: {
-            uid: 'kdj23jkdjf',
-            displayName: "Davey",
-            email: 'davey@fretless.com'
-        },
-        body: 'Too much chatting.'
-        }
-    ]
-
+const MessageList = (props) => {
     return (
         <div className='MessageList'>
-            { messages.map(msg => 
+            { props.messages.map(msg => 
             <Message message={msg} key={msg.id}/>)}
         </div>
     )
