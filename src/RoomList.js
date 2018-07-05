1cbfbdaf51ee9cb3c97c64ed2b8ae3ab894b31b4
Andrew Burkholder <hi@andrew.vip>
2018-07-05 09:25:29 -0400

    add styles to UserInfo and RoomList from static css files

--- src/RoomList.js
+++ src/RoomList.js
@@ -15,4 +15,32 @@ const RoomList = (props) => {
     )
 }
 
+const styles = {
+    ".RoomList h2" : {
+        fontSize: '1rem',
+      },
+      
+      ".RoomList ul" : {
+        listStyle: 'none',
+        marginLeft: 0,
+        paddingLeft: 0,
+      },
+      
+      ".RoomList li" : {
+        marginBottom: '0.5rem',
+      },
+      ".RoomList li a" : {
+        display: 'block',
+        color: 'whitesmoke',
+        textDecoration: 'none',
+      },
+      ".RoomList li a::before" : {
+        content: '# ',
+      },
+      ".RoomList li a:hover" : {
+        backgroundColor: 'rgba(255, 255, 255, 0.2)',
+      },
+      
+}
+
 export default RoomList
--- src/UserInfo.js
+++ src/UserInfo.js
@@ -17,4 +17,30 @@ const UserInfo = (props) => {
     )
 }
 
+const styles = {
+    '.UserInfo' : {
+        marginBottom: '1rem',
+        display: 'flex',
+        alignItems: 'center',
+      },
+      '.UserInfo .Avatar' : {
+        marginRight: '0.5rem',
+      },
+      '.UserInfo .user' : {
+        flex: 1,
+      },
+      '.UserInfo a' : {
+        border: 0,
+        padding: 0,
+        backgroundColor: 'transparent',
+        color: 'rgba(255, 255, 255, 0.6)',
+        fontSize: '1.2rem',
+        transition: 'color 0.25s ease-out',
+      },
+      '.UserInfo a:hover' : {
+        color: 'white',
+      },
+      
+}
+
 export default UserInfo
