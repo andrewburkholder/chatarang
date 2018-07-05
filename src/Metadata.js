e623be42af916a30ea53c97ae484a204b5ce19a2
Andrew Burkholder <hi@andrew.vip>
2018-07-05 14:41:30 -0400

    style metadata component

--- src/Metadata.js
+++ src/Metadata.js
@@ -1,29 +1,36 @@
 import React from 'react'
 
-const Metadata = (props) => {
-    return(
-        <div className="Metadata" style={styles[".Metadata"]}>
-            <div className="user" style={styles[".Metadata .user"]}>{props.displayName}</div>
-            <div className="time" style={styles[".Metadata .time"]}>1:10 PM</div>
-        </div>
-    )
+const Metadata = ({ message }) => {
+  return (
+    <div
+      className="Metadata"
+      style={styles.metadata}
+    >
+      <div style={styles.user}>
+        {message.user.displayName}
+      </div>
+      <div style={styles.time}>
+        1:10 PM
+      </div>
+    </div>
+  )
 }
 
 const styles = {
-    '.Metadata' : {
-        display: 'flex',
-        alignItems: 'baseline',
-      },
-      
-      '.Metadata .user' : {
-        fontWeight: 'bold',
-        marginRight: '0.5rem',
-      },
-      
-      '.Metadata .time' : {
-        color: '#999',
-        fontSize: '0.8rem',
-      },
-      
+  metadata: {
+    display: 'flex',
+    alignItems: 'baseline',
+  },
+
+  user: {
+    fontWeight: 'bold',
+    marginRight: '0.5rem',
+  },
+
+  time: {
+    color: '#999',
+    fontSize: '0.8rem',
+  },
 }
+
 export default Metadata
