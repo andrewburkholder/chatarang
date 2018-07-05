f3c7de74a687d30d6272e1745cebd2784b65548a
Andrew Burkholder <hi@andrew.vip>
2018-07-05 13:46:33 -0400

    update UserInfo styles to match Davey's

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,46 +1,33 @@
 import React from 'react'
 
 const UserInfo = (props) => {
-    return(
-        <div
-        className="UserInfo"
-        style={props.styles.children}
-      >
-        <div className="Avatar"></div>
-        <div className="user">
-          {props.user.displayName}
-        </div>
-        <a href="#">
-          <i className="fas fa-sign-out-alt"></i>
-        </a>
+  return (
+    <div
+      className="UserInfo"
+      style={styles.userInfo}
+    >
+      <div className="Avatar"></div>
+      <div style={styles.user}>
+        {props.user.displayName}
       </div>
-    )
+      <a href="#">
+        <i className="fas fa-sign-out-alt"></i>
+      </a>
+    </div>
+  )
 }
 
 const styles = {
-    '.UserInfo' : {
-        marginBottom: '1rem',
-        display: 'flex',
-        alignItems: 'center',
-      },
-      '.UserInfo .Avatar' : {
-        marginRight: '0.5rem',
-      },
-      '.UserInfo .user' : {
-        flex: 1,
-      },
-      '.UserInfo a' : {
-        border: 0,
-        padding: 0,
-        backgroundColor: 'transparent',
-        color: 'rgba(255, 255, 255, 0.6)',
-        fontSize: '1.2rem',
-        transition: 'color 0.25s ease-out',
-      },
-      '.UserInfo a:hover' : {
-        color: 'white',
-      },
-      
+  userInfo: {
+    padding: '0 1rem',
+    marginBottom: '1rem',
+    display: 'flex',
+    alignItems: 'center',
+  },
+
+  user: {
+    flex: 1,
+  }
 }
 
 export default UserInfo
